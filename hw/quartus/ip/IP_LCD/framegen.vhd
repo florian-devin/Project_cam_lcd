6a88dd837c28b56f0fc482d5b30b0a4b7dd55288
Wimpakt <maxence.vouaillat@gmail.com>
2022-12-16 12:11:14 +0100

    framegen + TB progress

---
+++ hw/quartus/ip/IP_LCD/framegen.vhd
@@ -0,0 +1,110 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+--use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity framegen is
+	port(
+		clk : in std_logic;
+		nReset : in std_logic;
+
+		config : in std_logic_vector(15 downto 0);
+        data : in std_logic_vector(15 downto 0);
+        FifoEmpty : in std_logic;
+        pendingCfg : in std_logic;
+        D_CX_IN : in std_logic;
+		
+        frame_finished : out std_logic;
+        CSX : out std_logic;
+        D_CX : out std_logic;
+        WRX : out std_logic;
+        RDX : out std_logic;
+        D : out std_logic_vector(15 downto 0)
+		);
+end framegen;
+
+architecture RTL of framegen is
+
+	type states is (idle, send1, send2, send3, send4);
+	signal current_state: states;
+	attribute enum_encoding: string;
+	attribute enum_encoding of states: type is "gray";
+
+	signal frame_finished_i : std_logic := '0';
+	signal CSX_i : std_logic := '0';
+	signal D_CX_i : std_logic := '0';
+    signal WRX_i : std_logic := '0';
+    signal RDX_i : std_logic := '0';
+    signal D_i : std_logic_vector(15 downto 0) := (others => '0');
+    signal D_memorized : std_logic_vector(15 downto 0) := (others => '0');
+begin
+
+
+	process(clk, nReset, current_state)
+	begin
+    		if nReset = '0' then
+                frame_finished_i <= '0';
+                CSX_i <= '1'; -- Not sure about the initial value
+                D_CX_i <= '0';
+                WRX_i <= '0';
+                RDX_i <= '0';
+                D_i <= (others => '0');
+        		current_state <= idle;
+				
+    		elsif rising_edge(clk) then
+
+				case current_state is
+		
+				when idle =>
+                    frame_finished_i <= '1';
+                    CSX_i <= '1';
+                    D_CX_i <= '0';
+                    WRX_i <= '0';
+                    RDX_i <= '1';
+                    --D_i <= (others => '0');
+                    D_memorized <= (others => '0');
+					if pendingCfg = '1' then
+                        current_state <= send1;
+                        D_memorized <= config;
+                        frame_finished_i <= '0';
+                    elsif FifoEmpty = '0' then -- pendingCfg = '0' and FifoEmpty = '0'
+                        current_state <= send1;
+                        D_memorized <= data;
+                        frame_finished_i <= '0';
+					end if;
+		
+				when send1 =>
+					CSX_i <= '0';
+                    D_CX_i <= D_CX_IN;
+                    WRX_i <= '0';
+                    D_i <= D_memorized;
+                    current_state <= send2;
+
+                when send2 =>
+                    -- Second clock cycle of WRX at low
+                    current_state <= send3;
+
+                when send3 =>
+                    WRX_i <= '1';
+                    current_state <= send4;
+
+                when send4 =>
+                    -- Second clock cycle of WRX at high
+                    current_state <= idle;
+        
+                when others =>
+                    current_state <= idle;
+                end case;
+
+    		end if;
+	end process;
+
+    frame_finished <= frame_finished_i;
+    CSX <= CSX_i;
+    D_CX <= D_CX_i;
+    WRX <= WRX_i;
+    RDX <= RDX_i;
+    D <= D_i;
+
+end RTL;
+
