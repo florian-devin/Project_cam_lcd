bd5e53e217f079d7968131f894cab7faa9ba1718
florian-devin <florian.devin13@gmail.com>
2023-01-04 16:40:30 +0100

    add c program base

---
+++ sw/nios/application/CamInit.c
@@ -0,0 +1,55 @@
+// Program for camera configuration : Write camera registers using i2c module
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "i2c/i2c.h"
+
+bool trdb_d5m_write(i2c_dev *i2c, uint8_t register_offset, uint16_t data) {
+    uint8_t byte_data[2] = {(data >> 8) & 0xff, data & 0xff};
+
+    int success = i2c_write_array(i2c, TRDB_D5M_I2C_ADDRESS, register_offset, byte_data, sizeof(byte_data));
+
+    if (success != I2C_SUCCESS) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool trdb_d5m_read(i2c_dev *i2c, uint8_t register_offset, uint16_t *data) {
+    uint8_t byte_data[2] = {0, 0};
+
+    int success = i2c_read_array(i2c, TRDB_D5M_I2C_ADDRESS, register_offset, byte_data, sizeof(byte_data));
+
+    if (success != I2C_SUCCESS) {
+        return false;
+    } else {
+        *data = ((uint16_t) byte_data[0] << 8) + byte_data[1];
+        return true;
+    }
+}
+
+
+int Init_Cam(void) {
+
+    
+  //  i2c_dev i2c = i2c_inst((void *) TRDB_D5M_0_I2C_0_BASE);
+  //  i2c_init(&i2c, I2C_FREQ);
+//
+  //  bool success = true;
+//
+  //  /* write the 16-bit value 23 to register 10 */
+  //  success &= trdb_d5m_write(&i2c, 10, 23);
+//
+  //  /* read from register 10, put data in readdata */
+  //  uint16_t readdata = 0;
+  //  success &= trdb_d5m_read(&i2c, 10, &readdata);
+//
+  //  if (success) {
+  //      return EXIT_SUCCESS;
+  //  } else {
+  //      return EXIT_FAILURE;
+  //  }
+    
+}
---
+++ sw/nios/application/main.c
@@ -0,0 +1,16 @@
+// Main program for Lab4
+
+///////////////////////// INCLUDE
+#include <stdio.h>
+#include <inttypes.h>
+#include "io.h"
+#include "system.h"
+#include "CamInit.h"
+///////////////////////// END INCLUDE
+
+
+int main() 
+{
+
+
+}
