ae1dc11cf1078df944c2a89ecd74a4e2406cb025
florian-devin <florian.devin13@gmail.com>
2023-01-04 22:37:54 +0100

    ajout de la config lcd addr

--- sw/nios/application/main.c
+++ sw/nios/application/main.c
@@ -12,6 +12,7 @@
 int main() 
 {
     Init_Cam(void);
+    IOWR_32DIRECT(IP_PWM_0_BASE, 0x06,0x18); // camLCDaddr
     IOWR_32DIRECT(IP_PWM_0_BASE, 0x00,0x0); // camAddr
     IOWR_32DIRECT(IP_PWM_0_BASE, 0x01,240*320/2); // camLength
     IOWR_8DIRECT(IP_PWM_0_BASE, 0x03,0x1); // Camstart
